f579bc1bc183a4d8221c4432ebb39c404b970b07
Tim Roes <mail@timroes.de>
2018-09-10 17:10:06 +0200

    Move to GitHub v4 GraphQL API

--- config.js
+++ config.js
@@ -8,10 +8,7 @@ if (!process.env.GITHUB_OAUTH_TOKEN || !process.env.ES_HOST || !process.env.ES_A
 	throw new Error('You need to specify GITHUB_OAUTH_TOKEN, ES_HOST and ES_AUTH env variables.');
 }
 
-const githubAuth = {
-	type: 'oauth',
-	token: process.env.GITHUB_OAUTH_TOKEN
-};
+const githubToken = process.env.GITHUB_OAUTH_TOKEN;
 
 const elasticsearch = {
 	host: process.env.ES_HOST,
@@ -20,6 +17,6 @@ const elasticsearch = {
 
 module.exports = {
 	elasticsearch,
-	githubAuth,
+	githubToken,
 	repos
 };
--- index.js
+++ index.js
@@ -1,29 +1,59 @@
+const fs = require('fs');
 const config = require('./config.js');
+const { GraphQLClient } = require('graphql-request');
 
-const octokit = require('@octokit/rest')();
+const query = fs.readFileSync('./query.graphql', 'utf8');
+
 const elasticsearch = require('elasticsearch');
 const moment = require('moment');
 
-const CACHE_INDEX = 'cache';
-
 const client = new elasticsearch.Client(config.elasticsearch);
 
-octokit.authenticate(config.githubAuth);
+const graphqlClient = new GraphQLClient('https://api.github.com/graphql', {
+  headers: {
+    'Authorization': `bearer ${config.githubToken}`
+  }
+});
 
 /**
  * Enhace a passed in date, into an object that contains further useful
  * information about that date (e.g. day of the week or hour of day).
  */
 function enhanceDate(date) {
-	if (!date) return null;
+  if (!date) return null;
 
-	const m = moment(date);
-	return {
-		time: m.format(),
-		weekday: m.format('ddd'),
-		weekday_number: parseInt(m.format('d')),
-		hour_of_day: parseInt(m.format('H'))
-	};
+  const m = moment(date);
+  return {
+    time: m.format(),
+    weekday: m.format('ddd'),
+    weekday_number: parseInt(m.format('d')),
+    hour_of_day: parseInt(m.format('H'))
+  };
+}
+
+function buildPrInfo(issue) {
+  const reviewers = issue.reviews.edges.map(({ review }) => review.author.login);
+  const uniqueReviewers = [...new Set(reviewers)];
+  return {
+    additions: issue.additions,
+    deletions: issue.deletions,
+    changed_files: issue.changedFiles,
+    commits: issue.commits.totalCount,
+    merged: issue.merged,
+    merged_by: issue.mergedBy,
+    reviewers: uniqueReviewers,
+  };
+}
+
+function buildReactions(reactions) {
+  return {
+    upVote: reactions.find(r => r.content === 'THUMBS_UP').users.totalCount,
+    downVote: reactions.find(r => r.content === 'THUMBS_DOWN').users.totalCount,
+    laugh: reactions.find(r => r.content === 'LAUGH').users.totalCount,
+    hooray: reactions.find(r => r.content === 'HOORAY').users.totalCount,
+    confused: reactions.find(r => r.content === 'CONFUSED').users.totalCount,
+    heart: reactions.find(r => r.content === 'HEART').users.totalCount,
+  };
 }
 
 /**
@@ -31,39 +61,37 @@ function enhanceDate(date) {
  * object that should be stored inside Elasticsearch.
  */
 function convertIssue(owner, repo, raw) {
-	const time_to_fix = (raw.created_at && raw.closed_at) ?
-			moment(raw.closed_at).diff(moment(raw.created_at)) :
-			null;
-	return {
-		id: raw.id,
-		owner: owner,
-		repo: repo,
-		state: raw.state,
-		title: raw.title,
-		number: raw.number,
-		url: raw.url,
-		locked: raw.locked,
-		comments: raw.comments,
-		created_at: enhanceDate(raw.created_at),
-		updated_at: enhanceDate(raw.updated_at),
-		closed_at: enhanceDate(raw.closed_at),
-		author_association: raw.author_association,
-		user: raw.user.login,
-		body: raw.body,
-		labels: raw.labels.map(label => label.name),
-		is_pullrequest: !!raw.pull_request,
-		assignees: !raw.assignees ? null : raw.assignees.map(a => a.login),
-		reactions: !raw.reactions ? null : {
-			total: raw.reactions.total_count,
-			upVote: raw.reactions['+1'],
-			downVote: raw.reactions['-1'],
-			laugh: raw.reactions.laugh,
-			hooray: raw.reactions.hooray,
-			confused: raw.reactions.confused,
-			heart: raw.reactions.hearts,
-		},
-		time_to_fix: time_to_fix,
-	};
+  const isPr = Boolean(raw.pr);
+  const time_to_fix = (raw.createdAt && raw.closedAt) ?
+      moment(raw.closedAt).diff(moment(raw.createdAt)) :
+      null;
+  return {
+    id: raw.id,
+    owner: owner,
+    repo: repo,
+    state: isPr ? raw.prState : raw.state,
+    title: raw.title,
+    number: raw.number,
+    url: raw.url,
+    locked: raw.locked,
+    comments: raw.comments.totalCount,
+    created_at: enhanceDate(raw.createdAt),
+    updated_at: enhanceDate(raw.updatedAt),
+    closed_at: enhanceDate(raw.closedAt),
+    author_association: raw.authorAssociation,
+    user: raw.author.login,
+    body: raw.body,
+    labels: raw.labels.edges.map(({ label }) => label.name),
+    is_pullrequest: isPr,
+    assignees: raw.assignees.edges.length === 0 ? 
+        null : raw.assignees.edges.map(({ assignee }) => assignee.login),
+    reactions: {
+      total: raw.reactionCount.totalCount,
+      ...buildReactions(raw.reactions),
+    },
+    time_to_fix: time_to_fix,
+    pr: isPr ? buildPrInfo(raw) : null,
+  };
 }
 
 /**
@@ -71,22 +99,10 @@ function convertIssue(owner, repo, raw) {
  * which these issues should be stored.
  */
 function getIssueBulkUpdates(index, issues) {
-	return [].concat(...issues.map(issue => [
-		{ index: { _index: index, _type: 'doc', _id: issue.id }},
-		issue
-	]));
-}
-
-/**
- * Returns the bulk request body to update the cache key for the specified repo
- * and page.
- */
-function getCacheKeyUpdate(owner, repo, page, key) {
-	const id = `${owner}_${repo}_${page}`
-	return [
-		{ index: { _index: CACHE_INDEX, _type: 'doc', _id: id }},
-		{ owner, repo, page, key }
-	];
+  return [].concat(...issues.map(issue => [
+    { index: { _index: index, _type: 'doc', _id: issue.id }},
+    issue
+  ]));
 }
 
 /**
@@ -94,86 +110,36 @@ function getCacheKeyUpdate(owner, repo, page, key) {
  * This will convert all issues to the desired format, store them into
  * Elasticsearch and update the cache key, we got from GitHub.
  */
-async function processGitHubIssues(owner, repo, response, page) {
-	console.log(`Found ${response.data.length} issues`);
-	if (response.data.length > 0) {
-		const issues = response.data.map(issue => convertIssue(owner, repo, issue));
-		const bulkIssues = getIssueBulkUpdates(`issues-${owner}-${repo}`, issues);
-		const updateCacheKey = getCacheKeyUpdate(owner, repo, page, response.meta.etag);
-		const body = [...bulkIssues, ...updateCacheKey];
-		console.log('Writing issues and new cache key to Elasticsearch');
-		await client.bulk({ body });
-	}
+async function processGitHubIssues(owner, repo, response) {
+  const issues = response.issues.edges.map(({ issue }) => convertIssue(owner, repo, issue));
+  const bulkIssues = getIssueBulkUpdates(`issues-${owner}-${repo}`, issues);
+  console.log('Writing issues to Elasticsearch');
+  await client.bulk({ body: bulkIssues });
+  console.log('Finished writing issues to Elasticsearch');
 }
 
-/**
- * Load the existing cache for the specified repository. The result will be
- * in the format { [pageNr]: 'cacheKey' }.
- */
-async function loadCacheForRepo(owner, repo) {
-	const entries = await client.search({
-		index: CACHE_INDEX,
-		_source: ['page', 'key'],
-		size: 10000,
-		body: {
-			query: {
-				bool: {
-					filter: [
-						{ match: { owner } },
-						{ match: { repo } }
-					]
-				}
-			}
-		}
-	});
+async function processRepos() {
+  for (const repository of config.repos) {
+    console.log(`Processing repository ${repository}`);
+  	const [ owner, repo ] = repository.split('/');
+    let cursor = null;
+    let hasNextPage = true;
+    while (hasNextPage) {
+      console.log(`Process page for cursor: ${cursor}`);
 
-	if (entries.hits.total === 0) {
-		return {};
-	}
+      const response = await graphqlClient.request(query, {
+        query: `repo:"${repository}"`,
+        cursor: cursor,
+      });
 
-	return entries.hits.hits.reduce((cache, entry) => {
-		cache[entry._source.page] = entry._source.key;
-		return cache;
-	}, {});
+      const { cost, remaining, limit } = response.rateLimit;
+      console.log(`Rate limit: ${remaining}/${limit} (Cost: ${cost})`);
+      await processGitHubIssues(owner, repo, response);
+      cursor = response.issues.pageInfo.endCursor;
+      hasNextPage = response.issues.pageInfo.hasNextPage;
+    }
+    console.log(`Finished processing ${repository}`);
+  }
 }
 
-config.repos.forEach(async (repository) => {
-	console.log(`Processing repository ${repository}`);
-	const [ owner, repo ] = repository.split('/');
-
-	const cache = await loadCacheForRepo(owner, repo);
-
-	let page = 0;
-	let shouldCheckNextPage = true;
-	while(shouldCheckNextPage) {
-		page++;
-		console.log(`Requesting issues page ${page} for ${repository} (using etag ${cache[page]})`)
-		try {
-			const headers = cache[page] ? { 'If-None-Match': cache[page] } : {};
-			const response = await octokit.issues.getForRepo({
-				owner,
-				repo,
-				page,
-				per_page: 100,
-				state: 'all',
-				sort: 'created',
-				direction: 'asc',
-				headers: headers
-			});
-			console.log('Remaining request limit: %s/%s',
-				response.meta['x-ratelimit-remaining'],
-				response.meta['x-ratelimit-limit']
-			);
-			await processGitHubIssues(owner, repo, response, page);
-			shouldCheckNextPage = octokit.hasNextPage(response);
-		} catch (error) {
-			if (error.name === 'HttpError' && error.code === 304) {
-				// Ignore not modified responses and continue with the next page.
-				console.log('Page was not modified. Continue with next page.');
-				continue;
-			} else {
-				throw error;
-			}
-		}
-	}
-});
+processRepos();
